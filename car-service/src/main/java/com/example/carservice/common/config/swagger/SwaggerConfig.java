09fa0e15c945b8c7b15b1b5eeac83211478af138
kpopov <kpopov@multamedio.de>
2025-10-07 16:45:26 +0200

    feat: add custom dokumentation description

--- car-service/src/main/java/com/example/carservice/common/config/swagger/SwaggerConfig.java
+++ car-service/src/main/java/com/example/carservice/common/config/swagger/SwaggerConfig.java
@@ -3,11 +3,19 @@ package com.example.carservice.common.config.swagger;
 import java.text.Normalizer;
 import java.util.Comparator;
 import java.util.stream.Collectors;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
+@Profile("swagger")
 public class SwaggerConfig {
 
     /**
@@ -26,6 +34,27 @@ public class SwaggerConfig {
                 );
             }
         };
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI()
+    {
+        return new OpenAPI()
+                .info(new Info().title("Car Service")
+                        .version("1")
+                        .description("""
+                                Welcome to the Car Service REST-API documentation v1.\
+                                
+
+                                Most endpoints can be executed by using the Try out function of swagger. You can find this function on each \
+                                endpoint specification. The request can be configured by changing the available parameter values. \
+                                Please be extremely carefully with this function on production environments.
+                                
+                                Currently, there is no authentification for the endpoints necessary.""")
+                        .contact(new Contact()
+                                .name("Car Wash Service Haris")
+                                .url("https://carwash-haris.example.com")
+                                .email("carwash-haris.example.com")));
     }
 
     /**
